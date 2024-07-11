f75c5a22df18a8d0ec182053089a56d767511880
Marcos Paulo <marcospaullo552@gmail.com>
2024-07-11 18:33:01 -0300

    config cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from "fastify";
+import cors from '@fastify/cors'
 
 import { createTrip } from "./routes/create-trip";
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
 
 const app = fastify()
+
+app.register(cors, {
+  origin: '*' //url da aplicação front
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler)
